996d3a9abe8b382155eaaa0565e3f23e047cd003
韩德勤 <handeqin@handeqindeMacBook-Pro.local>
2022-03-03 21:48:24 +0800

    添加加密插件，添加网络请求插件，添加本地存储文件，网络请求文件初始化

--- src/views/dashboard/dashboard.vue
+++ src/views/dashboard/dashboard.vue
@@ -1,5 +1,11 @@
 <template>
-  <n-button>naive-ui</n-button>
+  <div class="dashboard">
+    <p class="mt-20px">
+      本项目采用 Vue3 + Vue Route4 + Vite + Typescript + Pinia + Naive UI +
+      Axios + Sass 开发
+    </p>
+    <p class="mt-20px">加密采用Blueimp-md5插件对数据进行加密</p>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -13,7 +19,12 @@ export default defineComponent({
 });
 </script>
 <style lang="scss">
-.test {
-  color: $test-color;
+.dashboard {
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  font-size: 14px;
+  font-weight: 600;
 }
 </style>
