a58e67102fe0a4d1e1ef0a6c0ed3814904aabecc
haqsir@163.com <haqsir@163.com>
2022-03-04 09:19:38 +0800

    项目配置文件调整

--- src/layout/Header/projectSetting/projectSetting.vue
+++ src/layout/Header/projectSetting/projectSetting.vue
@@ -1,13 +1,28 @@
 <template>
   <n-drawer v-model:show="isDrawer" :width="width" placement="right">
     <n-drawer-content :title="title">
-      <n-divider title-placement="center">主题</n-divider>
+      <n-divider title-placement="center">系统主题</n-divider>
+
+      <div class="drawer-setting-item align-items-top">
+          <span
+            class="theme-item"
+            v-for="(item, index) in appThemeList"
+            :key="index"
+            :style="{ 'background-color': item }"
+            @click="toggleTheme(item)"
+          >
+            <n-icon size="12" v-if="item === designStore.appTheme">
+              <CheckOutlined />
+            </n-icon>
+          </span>
+        </div>
       
     </n-drawer-content>
   </n-drawer>
 </template>
 <script lang="ts">
 import { defineComponent, toRefs, reactive } from "vue";
+import { appThemeList } from "@/config/projectSetting"
 export default defineComponent({
   name: "ProjectSetting",
   props: {
@@ -33,11 +48,17 @@ export default defineComponent({
     function closeDrawer() {
       state.isDrawer = false;
     }
+    function toggleTheme(item: string){
+        console.log(item);
+        
+    } 
 
     return {
       ...toRefs(state),
+      appThemeList,
       openDrawer,
       closeDrawer,
+      toggleTheme,
     };
   },
 });
