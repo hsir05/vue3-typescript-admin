b868d3887c8eeb75b32e2c5ee65b0a74aa3a5ccc
韩德勤 <handeqin@handeqindeMacBook-Pro.local>
2022-03-05 14:14:37 +0800

    vite.config 文件优化

---
+++ build/utils.ts
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+
+export function isDevFn(mode: string): boolean {
+  return mode === 'development';
+}
+
+export function isProdFn(mode: string): boolean {
+  return mode === 'production';
+}
+
+export function getEnvConfig(match = 'VITE_GLOB_', confFiles = ['.env', '.env.production']) {
+  let envConfig = {};
+  confFiles.forEach((item) => {
+    try {
+      const env = dotenv.parse(fs.readFileSync(path.resolve(process.cwd(), item)));
+      envConfig = { ...envConfig, ...env };
+    } catch (error) {}
+  });
+
+  Object.keys(envConfig).forEach((key) => {
+    const reg = new RegExp(`^(${match})`);
+    if (!reg.test(key)) {
+      Reflect.deleteProperty(envConfig, key);
+    }
+  });
+  return envConfig;
+}
+
+export function wrapperEnv(envConf: Recordable): ViteEnv {
+  const ret: any = {};
+
+  for (const envName of Object.keys(envConf)) {
+    let realName = envConf[envName].replace(/\\n/g, '\n');
+    realName = realName === 'true' ? true : realName === 'false' ? false : realName;
+
+    if (envName === 'VITE_PORT') {
+      realName = Number(realName);
+    }
+    if (envName === 'VITE_PROXY') {
+      try {
+        realName = JSON.parse(realName);
+      } catch (error) {}
+    }
+    ret[envName] = realName;
+    process.env[envName] = realName;
+  }
+  return ret;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,83 +1,45 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import type { UserConfig, ConfigEnv } from 'vite';
+import { wrapperEnv } from "./build/utils";
+import { createVitePlugins } from "./build/vite/plugin";
 import { resolve } from "path";
-import Components from "unplugin-vue-components/vite";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
-import { visualizer } from "rollup-plugin-visualizer";
-import compressPlugin from "vite-plugin-compression";
-import { createHtmlPlugin } from "vite-plugin-html";
-import WindiCSS from 'vite-plugin-windicss'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import path from 'path'
+import { loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [
-        NaiveUiResolver(),
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+    const root = process.cwd();
+  const env = loadEnv(mode, root);
+  const viteEnv = wrapperEnv(env);
+  const { VITE_PUBLIC_PATH, VITE_PORT } = viteEnv;
+  const isBuild = command === "build";
+
+  return {
+    base: VITE_PUBLIC_PATH,
+    plugins: createVitePlugins(viteEnv, isBuild),
+    resolve: {
+      alias: [
+        {
+          find: /\/#\//,
+          replacement: resolve(process.cwd(), ".", "types") + "/",
+        },
+        {
+          find: "@",
+          replacement: resolve(process.cwd(), ".", "src") + "/",
+        },
       ],
-    }),
-    visualizer({
-      open: true, // 是否直接打开报告
-      gzipSize: true, // 是否显示 gzipSize
-      brotliSize: false, // 是否显示brotliSize
-    }),
-    compressPlugin({
-      ext: ".gz", // 选择压缩后的文件名
-      algorithm: "gzip", // 何中方式压缩 brotliCompress gzip
-      deleteOriginFile: false, // 是否删除原文件
-    }),
-    createHtmlPlugin({
-      minify: true,
-      /**
-       * 在这里写entry后，你将不需要在`index.html`内添加 script 标签，原有标签需要删除
-       * @default src/main.ts
-       */
-      entry: "src/main.ts",
-      /**
-       * 如果你想将 `index.html`存放在指定文件夹，可以修改它，否则不需要配置
-       * @default index.html
-       */
-      //   template: 'public/index.html',
-
-      /**
-       * 需要注入 index.html ejs 模版的数据
-       */
-      inject: {
-        data: {
-          title: "益民出行综合管理平台",
-          //   injectScript: `<script src="./inject.js"></script>`,
+      dedupe: ["vue"],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/var.scss";',
         },
       },
-    }),
-    WindiCSS(),
-    createSvgIconsPlugin({
-        iconDirs: [path.resolve(process.cwd(), 'src/icons')],
-        symbolId: 'icon-[dir]-[name]'
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /\/#\//,
-        replacement: resolve(process.cwd(), ".", "types") + "/",
-      },
-      {
-        find: "@",
-        replacement: resolve(process.cwd(), ".", "src") + "/",
-      },
-    ],
-    dedupe: ["vue"],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/assets/styles/var.scss";',
+    },
+    server: {
+       host: true,
+      port: VITE_PORT,
+      proxy: {
+        "/api": "http://localhost:8888/",
       },
     },
-  },
-  server: {
-    host: "0.0.0.0",
-  },
-});
+  };
+};
