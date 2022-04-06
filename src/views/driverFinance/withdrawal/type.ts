3f86e823c92cab6888b2087f1b466302a7de90a2
韩德勤 <handeqin@handeqindeMacBook-Pro.local>
2022-04-06 21:59:46 +0800

    司机提现设置功能开发

---
+++ src/views/driverFinance/withdrawal/type.ts
@@ -0,0 +1,9 @@
+export interface tableDataItem{
+    id?: string
+    time: null | string[]
+    time_start: null | string
+    time_end: null | string
+    count: null | number
+    limit: null | number
+    descript: null | string
+}
