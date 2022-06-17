e8540da981c92d9920a7f70c9647a37d387fd011
haqsir@163.com <haqsir@163.com>
2022-06-17 13:39:40 +0800

    修改第三方接口contenttype类型

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -359,6 +359,7 @@ export const otherHttp = createAxios({
     apiUrl: otherUrl
   },
   headers: {
+    crossDomain: false,
     Accept: ContentTypeEnum.JSON, "Content-Type": ContentTypeEnum.FORM_URLENCODED,
   },
   withCredentials: false,
